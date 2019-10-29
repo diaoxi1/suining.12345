9e8eed63100a6143f1486b97f904858199a6a16e
diaoxin <448574806@qq.com>
2019-10-29 17:40:47 +0800

    初始化项目

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,23 @@
     <van-nav-bar title="12345政府服务热线">
       <van-icon name="user-o" slot="right" size="2em"/>
     </van-nav-bar>
-    <div class="banner">
-
+    <div class="banner van-hairline--bottom">
+      <img src="../assets/logo2.png" class="logo2">
+      <div class="search">
+        <input class="search-input" type="text">
+        <img src="../assets/sousuo.png" alt="" class="sousuo">
+      </div>
     </div>
     <van-row class="item-array">
-      <van-col span="8"></van-col>
-      <van-col span="8"></van-col>
-      <van-col span="8"></van-col>
+      <van-col span="8" class="item van-hairline--bottom">
+        <img src="../assets/xjcx.png" alt="">
+      </van-col>
+      <van-col span="8" class="item van-hairline--bottom van-hairline--left">
+        <img src="../assets/sjxx.png" alt="">
+      </van-col>
+      <van-col span="8" class="item van-hairline--bottom van-hairline--left">
+        <img src="../assets/swxx.png" alt="">
+      </van-col>
     </van-row>
 
   </div>
@@ -19,15 +29,63 @@
 
 export default {
   name: 'home',
+  data(){
+    return {
+      value:''
+    }
+  }
 }
 </script>
 <style>
   .banner{
-    height: 5em;
+    height: 123px;
     width: 100%;
-    background: red;
+    overflow: hidden;
+    position: relative;
+  }
+  .logo2{
+    position: absolute;
+    left: 50%;
+    top: 15px;
+    transform: translateX(-50%);
+    width: 126px;
+    height: 34px;
+  }
+  .search{
+    width: 80%;
+    margin:53.3px 0 0 10%;
+    position: relative;
+  }
+  .search-input{
+    outline:none;
+    width: 100%;
+    height: 44px;
+    margin: 0;
+    line-height: 44px;
+    font-size: 14px;
+    border: 1px solid #989898;
+  }
+  .sousuo{
+    position: absolute;
+    top: 50%;
+    height: 24px;
+    width: 24px;
+    right: 8px;
+    transform: translateY(-50%);
   }
   .item-array{
-    height: 2em;
+    height: 80px;
+  }
+  .item-array .item{
+    height: 100%;
+    position: relative;
+  }
+  .item-array .item img{
+    height: 54px;
+    width: 54px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
   }
 </style>
